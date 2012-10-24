9808814f0a3fa64693ebdd38ad92036d24a71606
leipan <lei.pan@jpl.nasa.gov>
2012-10-24 06:43:52 +0000

    plot to file
    git-svn-id: file:///repo/trunk@19 7b6b0737-f4d6-4764-b6e6-6b822f93333f

--- sandbox/example1.m
+++ sandbox/example1.m
@@ -1,14 +1,19 @@
+#! /home/sflops/local/packages/octave/bin/octave -qf
 x=[0,.11,.15,.18,.25,.32,.35,.39,.42,.44,.48,.50];
 y = [0 , 1 , 3 , 5 , 12 , 10 , 9 , 7 , 5 , 3 , 7 , 10];
 figure(1)
-plot(x,y);
+p1 = plot(x,y);
+print -dpng p1.png
+
 x1=linspace(0,5,.05);
 y1 = spline(x,y,x1)
 figure(2)
-plot(x1,y1);
+p2 = plot(x1,y1);
 axis([0, .5, 0, 15])
+print -dpng p2.png
 
 y2 = interp1(x,y,x1)
 figure(3)
-plot(x1,y2);
+p3 = plot(x1,y2);
 axis([0, .5, 0, 15])
+print -dpng p3.png
--- sandbox/plot1.m
+++ sandbox/plot1.m
@@ -1,3 +1,6 @@
+#! /home/sflops/local/packages/octave/bin/octave -qf
 x = linspace(0, 2*pi, 100);
 y = sin(x);
-plot(x, y);
+p1 = plot(x, y);
+
+print -dpng fig1.png
