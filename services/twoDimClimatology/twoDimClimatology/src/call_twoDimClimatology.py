94861fddffb4562362d1b4bb46ea2c997dd71999
leipan <lei.pan@jpl.nasa.gov>
2012-12-09 00:37:16 +0000

    added two input arguments
    git-svn-id: file:///repo/trunk@75 7b6b0737-f4d6-4764-b6e6-6b822f93333f

--- services/twoDimClimatology/twoDimClimatology/src/call_twoDimClimatology.py
+++ services/twoDimClimatology/twoDimClimatology/src/call_twoDimClimatology.py
@@ -5,14 +5,16 @@ import os
 from os.path import basename
 
 class call_twoDimClimatology:
-    def __init__(self, model='ukmo'):
+    def __init__(self, model='ukmo', inputf=None, outputf=None):
         self.model = model
+        self.inputf = inputf
+        self.outputf = outputf
 
     def displayTwoDimClimatology(self):
         ### print 'current file: ', os.path.realpath(__file__)
         ### print 'current dir: ', os.getcwd()
         ### command = '/home/svc/cmac/trunk/services/twoDimClimatology/twoDimClimatology/src/octaveWrapper ' + self.model
-        command = './octaveWrapper ' + self.model
+        command = './octaveWrapper ' + self.model + ' ' + self.inputf + ' ' + self.outputf
         cmd = command.split(' ')
         cmdstring = string.join(cmd, ' ')
         print 'cmdstring: ', cmdstring
@@ -30,7 +32,8 @@ class call_twoDimClimatology:
 
 
 if __name__ == '__main__':
-    c1 = call_twoDimClimatology('ukmo')
+    input_file = '/home/svc/cmac/trunk/services/twoDimClimatology/twoDimClimatology/static/ts_Amon_HadGEM2-A_amip_r1i1p1_197809-200811.nc'
+    c1 = call_twoDimClimatology('ukmo', input_file, 'ukmo_ts.jpeg')
 
     mesg = c1.displayTwoDimClimatology()
     print 'mesg: ', mesg
