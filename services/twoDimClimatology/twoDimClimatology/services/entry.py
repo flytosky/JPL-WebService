23632ca425db108c25f7b0839205b0771a42090e
leipan <lei.pan@jpl.nasa.gov>
2012-12-10 04:13:58 +0000

    url init value; added comments
    git-svn-id: file:///repo/trunk@85 7b6b0737-f4d6-4764-b6e6-6b822f93333f

--- services/twoDimClimatology/twoDimClimatology/services/entry.py
+++ services/twoDimClimatology/twoDimClimatology/services/entry.py
@@ -14,6 +14,7 @@ def display():
     # status and message
     success = True
     message = "ok"
+    url = ''
 
     # get model, input_file, output_file
     model = request.args.get('model', 'ukmo')
@@ -21,21 +22,27 @@ def display():
     image_file = request.args.get('image', '')
 
     try:
-      # chdir to where the app is
+      # get where the input file and output file are
       current_dir = os.getcwd()
       print 'current_dir: ', current_dir
       data_file = current_dir + '/twoDimClimatology/' + data_file
       image_file1 = current_dir + '/twoDimClimatology/' + image_file
+
+      # chdir to where the app is
       os.chdir(current_dir+'/twoDimClimatology/src')
+      # instantiate the app. class
       c1 = call_twoDimClimatology.call_twoDimClimatology(model, data_file, image_file1)
+      # call the app. function
       message = c1.displayTwoDimClimatology()
       # chdir back
       os.chdir(current_dir)
 
+      url = 'http://oscar2.jpl.nasa.gov:8088' + image_file
+
       if message.find('Error') >= 0:
         success = False
+        url = ''
 
-      url = 'http://oscar2.jpl.nasa.gov:8088' + image_file
     except ValueError, e:
         success = False
         message = str(e)
