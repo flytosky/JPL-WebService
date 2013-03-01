fe51bfba3dfa00145f10ac4b5f39bba2869eb8b0
leipan <lei.pan@jpl.nasa.gov>
2013-03-01 00:13:37 +0000

    in progress
    git-svn-id: file:///repo/trunk@122 7b6b0737-f4d6-4764-b6e6-6b822f93333f

--- services/twoDimClimatology/twoDimClimatology/services/entry.py
+++ services/twoDimClimatology/twoDimClimatology/services/entry.py
@@ -1,43 +1,54 @@
 import os, hashlib, shutil
 from datetime import datetime
+import md5
 
 from flask import jsonify, request, url_for, make_response
 from werkzeug import secure_filename
 
-from twoDimClimatology import app
-from twoDimClimatology.src import call_twoDimClimatology
+from twoDimMap import app
+from twoDimMap.src import call_twoDimMap
 
-@app.route('/twoDimClimatology/display', methods=["GET"])
+@app.route('/twoDimMap/display', methods=["GET"])
 def display():
-    """Run displayTwoDimClimatology"""
+    """Run displayTwoDimMap"""
 
     # status and message
     success = True
     message = "ok"
     url = ''
 
-    # get model, input_file, output_file
-    model = request.args.get('model', 'ukmo')
-    data_file = request.args.get('data', '')
-    image_file = request.args.get('image', '')
+    # get model, var, start time, end time, lon1, lon2, lat1, lat2, months
+
+    model = request.args.get('model', '')
+    var = request.args.get('var', '')
+    startT = request.args.get('start_time', '')
+    endT = request.args.get('end_time', '')
+    lon1 = request.args.get('lon1', '')
+    lon2 = request.args.get('lon2', '')
+    lat1 = request.args.get('lat1', '')
+    lat2 = request.args.get('lat2', '')
+    months = request.args.get('months', '')
 
     try:
       # get where the input file and output file are
       current_dir = os.getcwd()
       print 'current_dir: ', current_dir
-      data_file = current_dir + '/twoDimClimatology/' + data_file
-      image_file1 = current_dir + '/twoDimClimatology/' + image_file
+      tag = md5.new(model+startT+endT+lon1+lat1).hexdigest()
+      output_dir = current_dir + '/twoDimMap/static/' + tag
+      print 'output_dir: ', output_dir
+      if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
       # chdir to where the app is
-      os.chdir(current_dir+'/twoDimClimatology/src')
+      os.chdir(current_dir+'/twoDimMap/src')
       # instantiate the app. class
-      c1 = call_twoDimClimatology.call_twoDimClimatology(model, data_file, image_file1)
+      c1 = call_twoDimMap.call_twoDimMap(model, var, startT, endT, lon1, lon2, lat1, lat2, months, output_dir)
       # call the app. function
-      message = c1.displayTwoDimClimatology()
+      message = c1.displayTwoDimMap()
       # chdir back
       os.chdir(current_dir)
 
-      url = 'http://oscar2.jpl.nasa.gov:8088' + image_file
+      url = 'http://cmacws.jpl.nasa.gov:8088/twoDimMap/static/' + tag
 
       if message.find('Error') >= 0:
         success = False
