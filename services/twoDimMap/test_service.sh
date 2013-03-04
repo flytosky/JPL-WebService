cc7f1f71bab25c82399ae8ca90f1a26a74e3702a
leipan <lei.pan@jpl.nasa.gov>
2013-03-04 07:26:21 +0000

    init
    git-svn-id: file:///repo/trunk@127 7b6b0737-f4d6-4764-b6e6-6b822f93333f

---
+++ services/twoDimMap/test_service.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl "http://cmacws.jpl.nasa.gov:8089/twoDimMap/display?model=ukmo_hadgem2-a&var=ts&start_time=199001&end_time=199512&lon1=0&lon2=100&lat1=-29&lat2=29&months=4,5,6,10,12"
