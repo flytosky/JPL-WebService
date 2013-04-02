69ecfbde765f5b87ee8f3cbd0818741134d732f2
leipan <lei.pan@jpl.nasa.gov>
2013-04-02 08:21:13 +0000

    minor
    git-svn-id: file:///repo/trunk@163 7b6b0737-f4d6-4764-b6e6-6b822f93333f

--- services/twoDimMap/test_service.sh
+++ services/twoDimMap/test_service.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl "http://cmacws.jpl.nasa.gov:8089/twoDimMap/display?model=ukmo_hadgem2-a&var=ts&start_time=199001&end_time=199512&lon1=0&lon2=100&lat1=-29&lat2=29&months=1,2,4,5,6,10,12"
+curl "http://cmacws.jpl.nasa.gov:8089/twoDimMap/display?model=UKMO_HadGEM2-ES&var=ts&start_time=199001&end_time=199512&lon1=0&lon2=20&lat1=-29&lat2=29&months=1,2,3,4,5,6,7,8,9,10,11,12"
