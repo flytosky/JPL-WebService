6426ac8dd2d1fad4bc3e8cf7c591dc4461b07c3b
leipan <lei.pan@jpl.nasa.gov>
2013-05-20 09:02:36 +0000

    added call_timeSeries2D()
    git-svn-id: file:///repo/trunk@220 7b6b0737-f4d6-4764-b6e6-6b822f93333f

--- services/svc/svc/services/entry.py
+++ services/svc/svc/services/entry.py
@@ -7,6 +7,7 @@ from werkzeug import secure_filename
 
 from svc import app
 from svc.src.twoDimMap import call_twoDimMap
+from svc.src.timeSeries2D import call_timeSeries2D
 
 from flask import current_app
 from functools import update_wrapper
@@ -106,6 +107,75 @@ def displayTwoDimMap():
       os.chdir(current_dir)
 
       url = 'http://cmacws.jpl.nasa.gov:8090/static/twoDimMap/' + tag + '/' + imgFileName
+
+      print 'message: ', message
+      if len(message) == 0 or message.find('Error') >= 0 or message.find('error:') >= 0 :
+        success = False
+        url = ''
+
+    except ValueError, e:
+        success = False
+        message = str(e)
+
+
+    return jsonify({
+        'success': success,
+        'message': message,
+        'url': url,
+    })
+
+
+@app.route('/svc/timeSeries2D', methods=["GET"])
+@crossdomain(origin='*')
+def display_timeSeries2D():
+    """Run display_timeSeries2D"""
+
+    # status and message
+    success = True
+    message = "ok"
+    url = ''
+
+    # get model, var, start time, end time, lon1, lon2, lat1, lat2
+
+    model = request.args.get('model', '')
+    var = request.args.get('var', '')
+    startT = request.args.get('start_time', '')
+    endT = request.args.get('end_time', '')
+    lon1 = request.args.get('lon1', '')
+    lon2 = request.args.get('lon2', '')
+    lat1 = request.args.get('lat1', '')
+    lat2 = request.args.get('lat2', '')
+
+    print 'model: ', model
+    print 'var: ', var
+    print 'startT: ', startT
+    print 'endT: ', endT
+    print 'lon1: ', lon1
+    print 'lon2: ', lon2
+    print 'lat1: ', lat1
+    print 'lat2: ', lat2
+
+    try:
+      # get where the input file and output file are
+      current_dir = os.getcwd()
+      print 'current_dir: ', current_dir
+      seed_str = model+var+startT+endT+lon1+lon2+lat1+lat2
+      tag = md5.new(seed_str).hexdigest()
+      output_dir = current_dir + '/svc/static/timeSeries2D/' + tag
+      print 'output_dir: ', output_dir
+      if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+      # chdir to where the app is
+      os.chdir(current_dir+'/svc/src/timeSeries2D')
+      # instantiate the app. class
+      c1 = call_timeSeries2D.call_timeSeries2D(model, var, startT, endT, lon1, lon2, lat1, lat2, output_dir)
+      # call the app. function
+      (message, imgFileName) = c1.display_timeSeries2D()
+      # chdir back
+      os.chdir(current_dir)
+
+      url = 'http://cmacws.jpl.nasa.gov:8090/static/timeSeries2D/' + tag + '/' + imgFileName
 
       print 'message: ', message
       if len(message) == 0 or message.find('Error') >= 0 or message.find('error:') >= 0 :
