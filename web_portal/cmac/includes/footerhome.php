ad241a23496b1f44071af81ecec965c960b6155b
leipan <lei.pan@jpl.nasa.gov>
2013-06-22 09:53:55 +0000

    added CL#
    git-svn-id: file:///repo/trunk@300 7b6b0737-f4d6-4764-b6e6-6b822f93333f

--- web_portal/cmac/includes/footerhome.php
+++ web_portal/cmac/includes/footerhome.php
@@ -4,7 +4,7 @@
 			<div class="footer_credits">
             
             
-<p>Site Manager: <a href="mailto:Seungwon.Lee@jpl.nasa.gov">Seungwon Lee</a><br/> JPL Clearance: CL#**-****</p>
+<p>Site Manager: <a href="mailto:Seungwon.Lee@jpl.nasa.gov">Seungwon Lee</a><br/> JPL Clearance: CL#13-1931</p>
 
   			</div>
   						<div class="footer_nav"><p><a href="http://www.jpl.nasa.gov/copyrights.cfm">PRIVACY</a> &nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;<a href="http://www.jpl.nasa.gov/imagepolicy/">IMAGE POLICY</a></p>
