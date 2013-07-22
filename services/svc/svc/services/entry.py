45dcf659610ed3d1b5c8e9ace6aeac3cd6e0ee9d
leipan <lei.pan@jpl.nasa.gov>
2013-07-22 10:04:27 +0000

    added displayDiffPlot2V()
    git-svn-id: file:///repo/trunk@410 7b6b0737-f4d6-4764-b6e6-6b822f93333f

--- services/svc/svc/services/entry.py
+++ services/svc/svc/services/entry.py
@@ -123,7 +123,7 @@ def displayTwoDimMap():
       # instantiate the app. class
       c1 = call_twoDimMap.call_twoDimMap(model, var, startT, endT, lon1, lon2, lat1, lat2, months, output_dir)
       # call the app. function
-      (message, imgFileName) = c1.displayTwoDimMap()
+      (message, imgFileName, dataFileName) = c1.displayTwoDimMap()
       # chdir back
       os.chdir(current_dir)
 
@@ -134,11 +134,14 @@ def displayTwoDimMap():
       ### url = 'http://cmacws.jpl.nasa.gov:8090/static/twoDimMap/' + tag + '/' + imgFileName
       url = 'http://' + hostname + ':' + port + '/static/twoDimMap/' + tag + '/' + imgFileName
       print 'url: ', url
+      dataUrl = 'http://' + hostname + ':' + port + '/static/twoDimMap/' + tag + '/' + dataFileName
+      print 'dataUrl: ', dataUrl
 
       print 'message: ', message
       if len(message) == 0 or message.find('Error') >= 0 or message.find('error:') >= 0 :
         success = False
         url = ''
+        dataUrl = ''
 
     except ValueError, e:
         success = False
@@ -149,6 +152,7 @@ def displayTwoDimMap():
         'success': success,
         'message': message,
         'url': url,
+        'dataUrl': dataUrl
     })
 
 
@@ -578,8 +582,8 @@ def displayScatterPlot2V():
       os.chdir(current_dir+'/svc/src/scatterPlot2V')
       # instantiate the app. class
       c1 = call_scatterPlot2V.call_scatterPlot2V(model1, var1, model2, var2, startT, endT, lon1, lon2, lat1, lat2, output_dir)
-      # call the app. function
-      (message, imgFileName) = c1.displayScatterPlot2V()
+      # call the app. function (0 means the image created is scatter plot)
+      (message, imgFileName) = c1.displayScatterPlot2V(0)
       # chdir back
       os.chdir(current_dir)
 
@@ -605,4 +609,83 @@ def displayScatterPlot2V():
         'message': message,
         'url': url,
     })
+
+
+@app.route('/svc/diffPlot2V', methods=["GET"])
+@crossdomain(origin='*')
+def displayDiffPlot2V():
+    """Run displayDiffPlot2V"""
+
+    # status and message
+    success = True
+    message = "ok"
+    url = ''
+
+    # get model1, var1,  model2, var2, start time, end time, lon1, lon2, lat1, lat2
+
+    model1 = request.args.get('model1', '')
+    var1 = request.args.get('var1', '')
+    model2 = request.args.get('model2', '')
+    var2 = request.args.get('var2', '')
+    startT = request.args.get('start_time', '')
+    endT = request.args.get('end_time', '')
+    lon1 = request.args.get('lon1', '')
+    lon2 = request.args.get('lon2', '')
+    lat1 = request.args.get('lat1', '')
+    lat2 = request.args.get('lat2', '')
+
+    print 'model1: ', model1
+    print 'var1: ', var1
+    print 'model2: ', model2
+    print 'var2: ', var2
+    print 'startT: ', startT
+    print 'endT: ', endT
+    print 'lon1: ', lon1
+    print 'lon2: ', lon2
+    print 'lat1: ', lat1
+    print 'lat2: ', lat2
+
+    try:
+      # get where the input file and output file are
+      current_dir = os.getcwd()
+      print 'current_dir: ', current_dir
+      seed_str = model1+var1+model2+var2+startT+endT+lat1+lat2+lon1+lon2
+      tag = md5.new(seed_str).hexdigest()
+      output_dir = current_dir + '/svc/static/diffPlot2V/' + tag
+      print 'output_dir: ', output_dir
+      if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+      # chdir to where the app is
+      os.chdir(current_dir+'/svc/src/scatterPlot2V')
+      # instantiate the app. class
+      c1 = call_scatterPlot2V.call_scatterPlot2V(model1, var1, model2, var2, startT, endT, lon1, lon2, lat1, lat2, output_dir)
+      # call the app. function (1 means the image created is difference plot)
+      (message, imgFileName) = c1.displayScatterPlot2V(1)
+      # chdir back
+      os.chdir(current_dir)
+
+      hostname, port = get_host_port("host.cfg")
+      print 'hostname: ', hostname
+      print 'port: ', port
+
+      url = 'http://' + hostname + ':' + port + '/static/diffPlot2V/' + tag + '/' + imgFileName
+      print 'url: ', url
+
+      print 'message: ', message
+      if len(message) == 0 or message.find('Error') >= 0 or message.find('error:') >= 0 :
+        success = False
+        url = ''
+
+    except ValueError, e:
+        success = False
+        message = str(e)
+
+
+    return jsonify({
+        'success': success,
+        'message': message,
+        'url': url,
+    })
+
 
