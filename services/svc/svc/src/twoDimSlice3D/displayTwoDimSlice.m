ad5b727d132657651deb775743cd6c09cb2c3471
chengxingzhai <Chengxing.Zhai@jpl.nasa.gov>
2013-08-07 23:30:52 +0000

    fixed the longitude range [-180, 0] problem
    git-svn-id: file:///repo/trunk@437 7b6b0737-f4d6-4764-b6e6-6b822f93333f

--- services/svc/svc/src/twoDimSlice3D/displayTwoDimSlice.m
+++ services/svc/svc/src/twoDimSlice3D/displayTwoDimSlice.m
@@ -95,8 +95,8 @@ for fileI = 1:nFiles
       else
         lonIdx = find(lon <= lonRange(2));
         lonIdx_neg = find(lon_neg >= lonRange(1));
-        lonIdx = [lonIdx_neg; lonIdx];
         lon = [lon_neg(lonIdx_neg); lon(lonIdx)];
+        lonIdx = [lonIdx_neg; lonIdx];
         nLon = length(lonIdx);
       end
     end
