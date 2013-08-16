073c9a73d4f9e531eeae668f8d2c56cbea909beb
chengxingzhai <Chengxing.Zhai@jpl.nasa.gov>
2013-08-16 23:58:23 +0000

    moved utility functions into a common directory
    git-svn-id: file:///repo/trunk@446 7b6b0737-f4d6-4764-b6e6-6b822f93333f

--- services/svc/svc/src/twoDimZonalMean/temporalRangeOverlap.m
+++
@@ -1,9 +0,0 @@
-function overlap = temporalRangeOverlap(startTime1, stopTime1, startTime2, stopTime2)
-%
-% This function determines whether two temporal intervals overlap.
-%
-% Author: Chengxing Zhai
-% Revision history:
-%   2012/12/03:	Initial version, cz
-%
-overlap = ~earlierThan(stopTime1, startTime2) & ~laterThan(startTime1, stopTime2);
