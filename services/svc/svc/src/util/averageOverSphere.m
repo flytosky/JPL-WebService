cd22b04a65853a3c39586c24be584d9fc54763c3
chengxingzhai <Chengxing.Zhai@jpl.nasa.gov>
2014-01-27 18:42:14 +0000

    updated to use new netcdf dimension convention
    git-svn-id: file:///repo/trunk@550 7b6b0737-f4d6-4764-b6e6-6b822f93333f

--- services/svc/svc/src/util/averageOverSphere.m
+++ services/svc/svc/src/util/averageOverSphere.m
@@ -1,7 +1,7 @@
 function avg = averageOverSphere(data, lat, latRange, lon, lonRange, w)
 %
 % This function computes the average of data over sphere assuming that the first two dimensions
-% are longitude and latitude respectively.
+% are longitude and latitude respectively. It excludes data points that do not have valid values, e.g. NaN Inf
 %
 % Input:
 %   data	-- lon x lat x optional dimensions
@@ -18,38 +18,37 @@ function avg = averageOverSphere(data, lat, latRange, lon, lonRange, w)
 
 dataSize = size(data);
 
-cosLat = cos(lat(:)*pi/180);
+cosLat = cos(lat*pi/180);
+
+if nargin > 5
+  w = w .* repmat(cosLat(:)', [dataSize(1),1]);
+else
+  w = repmat(cosLat(:)', [dataSize(1),1]);
+end
+
+repSize = dataSize; repSize(1:2) = 1; 
+w = repmat(w, repSize);
+
+w(~isfinite(data)) = 0;
+data(~isfinite(data)) = 0;
 
 if nargin > 4
   lonIdx = find(lon >= lonRange(1) & lon <= lonRange(2));
 else
-  lonIdx = 1:dataSize(end);
+  lonIdx = 1:dataSize(1);
 end
 
 if nargin > 2
   latIdx = find(lat >= latRange(1) & lat <= latRange(2));
 else
-  latIdx = 1:dataSize(end-1);
+  latIdx = 1:dataSize(2);
 end
 
-if nargin > 5
-  w = w(latIdx, lonIdx) .* repmat(cosLat(latIdx), [1,length(lonIdx)]);
-else
-  w = repmat(cosLat(latIdx), [1, length(lonIdx)]);
-end
+data = reshape(data(lonIdx, latIdx,:), [], prod(dataSize(3:end)));
+w = reshape(w(lonIdx, latIdx,:), [], prod(dataSize(3:end)));
 
-data = reshape(data, [prod(dataSize(1:(end-2))), dataSize((end-1):end)]);
-data = data(:,latIdx, lonIdx); 
-w = repmat(reshape(w, [1, dataSize((end-1):end)]), [prod(dataSize(1:(end-2))),1,1]);
-
-w(~isfinite(data)) = 0;
-data(~isfinite(data)) = 0;
-
-w = reshape(w, size(w,1), []);
-data = reshape(data, size(w,1), []);
-
-avg = squeeze(sum(w .* data, 2) ./ sum(w,2));
+avg = squeeze(sum(w .* data)) ./ sum(w);
 
 if length(dataSize) > 3
-  avg = reshape(avg, dataSize(1:(end-2)));
+  avg = reshape(avg, dataSize(3:end));
 end
