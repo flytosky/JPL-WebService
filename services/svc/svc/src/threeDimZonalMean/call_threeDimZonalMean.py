4e46526bb8f52ccce53306890be8b3e24164b4e5
chengxingzhai <Chengxing.Zhai@jpl.nasa.gov>
2014-03-03 23:35:05 +0000

    updated to incorporate display options
    git-svn-id: file:///repo/trunk@611 7b6b0737-f4d6-4764-b6e6-6b822f93333f

--- services/svc/svc/src/threeDimZonalMean/call_threeDimZonalMean.py
+++ services/svc/svc/src/threeDimZonalMean/call_threeDimZonalMean.py
@@ -78,7 +78,8 @@ class call_threeDimZonalMean:
 
 
 if __name__ == '__main__':
-    c1 = call_threeDimZonalMean('gfdl_cm3', 'cli', '197501', '199512', '-60', '60', '900', '200', '5,6,7,8', '/home/svc/cmac/trunk/services/threeDimZonalMean/threeDimZonalMean/static/', '7')
+    ### ./octaveWrapper gfdl_cm3 cli 197501 199512 '-60 60' '90000, 20000' '5,6,7,8' '/tmp' 7
+    c1 = call_threeDimZonalMean('gfdl_cm3', 'cli', '197501', '199512', '-60', '60', '90000', '20000', '5,6,7,8', './', '7')
 
     mesg = c1.displayThreeDimZonalMean()
     print 'mesg: ', mesg
