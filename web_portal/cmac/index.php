87520cdaa68705e4f01c3a90f6ab83ffaf73f763
leipan <lei.pan@jpl.nasa.gov>
2014-09-10 04:41:08 +0000

    version for summer school 2014
    git-svn-id: file:///repo/trunk@760 7b6b0737-f4d6-4764-b6e6-6b822f93333f

--- web_portal/cmac/index.php
+++ web_portal/cmac/index.php
@@ -4,63 +4,16 @@
 Parallel Web Services for Data Preparation and Data Analysis
 </h1>
 <h3>
-A NASA CMAC funded project
+Summer School 2014, JPL Center for Climate Sciences
 </h3>
 
-<p>
-This web portal provides a user access to a scientific system that we developed for the NASA CMAC (Computational Modeling Algorithms and Cyberinfrastructure) project. The scientific system enables multi-aspect physics-based and phenomenon-oriented model performance evaluations and diagnoses through the comprehensive and synergistic use of multiple observational data, reanalysis data, and model outputs.  The system streamlined and structured long and complex steps involved in processing multi-source heterogeneous datasets, and enhanced the computational efficiency and data-volume handling capacity for the large-volume data analysis problem. We developed a parallel, distributed web-service oriented system to achieve this goal. The developed system provides the following key capabilities:
-</p>
+<br />
 
-<ol>
-<li>
-<i>Parallel web-service data preparation of observation data and model outputs for model-data intercomparisons</i>, supporting the following operations:
-</li>
+<img src="http://climatesciences.jpl.nasa.gov/system/pages/images/16/medium/8703587212_446f000243_z.jpg" alt="NASA Earth missions, 2013" >
 
-<ul>
-<li>
-Change data format (e.g. change a HDF file to a NetCDF file).
-</li>
-<li>
-Subset data conditionally by time, space, and variable (e.g. select tropical summer water vapor data).
-</li>
-<li>
-Concatenate data from multiple files (e.g. collect precipitation rate from year 2000 to 2005).
-</li>
-<li>
-Change horizontal and vertical coordinates (e.g. change height to pressure level).
-</li>
-<li>
-Average and regrid the temporal and spatial resolution (e.g. monthly 1x1 degree averaged values).
-</li>
-<li>
-Co-locate and interpolate multi-source outputs to match time and locations (e.g. co-locate MODIS footprint data with CloudSat footprint data).
-</li>
-<li>
-Convert variable units (e.g. convert cloud water content from the density unit (mg/m3) to the mass fraction unit (mg/kg)).
-</li>
-<li>
-Check data format.
-</li>
-<li>
-Format data.
-</li>
 <p>
-<img src="images/service.jpeg" alt="service" height="60" width="60">
-<a href="http://cmacws.jpl.nasa.gov:8083/">CMIP5 Checker Service</a>.
+This web portal provides a user access to a scientific system that we developed for the NASA CMAC (Computational Modeling Algorithms and Cyberinfrastructure) project. The scientific system enables multi-aspect physics-based and phenomenon-oriented model performance evaluations and diagnoses through the comprehensive and synergistic use of multiple observational data, reanalysis data, and model outputs.  The system streamlined and structured long and complex steps involved in processing multi-source heterogeneous datasets, and enhanced the computational efficiency and data-volume handling capacity for the large-volume data analysis problem. We developed a parallel, distributed web-service oriented system to achieve this goal. The developed system supports the following operations:
 </p>
-<p>
-<img src="images/service.jpeg" alt="service" height="60" width="60">
-<a href="http://cmacws.jpl.nasa.gov:8081/">CMIP5 Formatter Service</a>.
-</p>
-<p>
-<img src="images/document.jpg" alt="document" height="60" width="60">
-<a href="http://obs4mips.llnl.gov:8080/wiki/requirements">Requirements for contributing data to Obs4MIPs</a>.
-</p>
-</ul>
-
-<li>
-<i>Parallel web-service data analysis for model performance evaluation and diagnosis</i>, supporting the following operations:
-</li>
 
 <ul>
 <li>
@@ -117,12 +70,6 @@ Sort data by a given variable condition (e.g. sort cloud water path data in orde
 <img src="images/service.jpeg" alt="service" height="60" width="60">
 <a href="/cmac/web/conditionalSampling.html">Conditional Sampling Service</a>.
 </p>
-<p>
-<img src="images/service.jpeg" alt="service" height="60" width="60">
-<a href="/cmac/web/co-location.html">Co-location Service</a>.
-</p>
 </ul>
-
-</ol>
 
 <?php require_once("./footer.php") ?>
