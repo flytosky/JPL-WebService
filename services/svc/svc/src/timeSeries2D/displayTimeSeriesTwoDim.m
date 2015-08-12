aab975d6878f9d548d19d52fde64b6178deeea44
chengxingzhai <Chengxing.Zhai@jpl.nasa.gov>
2015-08-12 16:09:20 +0000

    updated to use a common utility function
    git-svn-id: file:///repo/trunk@908 7b6b0737-f4d6-4764-b6e6-6b822f93333f

--- services/svc/svc/src/timeSeries2D/displayTimeSeriesTwoDim.m
+++ services/svc/svc/src/timeSeries2D/displayTimeSeriesTwoDim.m
@@ -65,15 +65,7 @@ for fileI = 1:nFiles
     monthlyData = nan(nMonths,1);
   end
 
-  v = single(ncread(thisFile, varName));
-  if hasAttribute(thisFile, varName, 'missing_value')
-    missingValue = ncreadatt(thisFile, varName, 'missing_value');
-    v(abs(v - missingValue) < 1) = NaN;
-  end
-  if hasAttribute(thisFile, varName, '_FillValue')
-    missingValue = ncreadatt(thisFile, varName, '_FillValue');
-    v(abs(v - missingValue) < 1) = NaN;
-  end
+  v = ncreadVar(thisFile, varName);
 
   v_units = ncreadatt(thisFile, varName, 'units');
   [startTime_thisFile, stopTime_thisFile] = parseDateInFileName(dataFile{fileI});
