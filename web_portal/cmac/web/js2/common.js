3f82690cb014d1a39a58b463c5a64f1917f3d4ea
benyang22 <benyang.tang@jpl.nasa.gov>
2015-08-19 02:26:36 +0000

    pulled the month selection code to common.js
    M    web/conditionalSampling.html
    M    web/conditionalSampling2Var.html
    M    web/js2/common.js
    
    
    git-svn-id: file:///repo/trunk@941 7b6b0737-f4d6-4764-b6e6-6b822f93333f

--- web_portal/cmac/web/js2/common.js
+++ web_portal/cmac/web/js2/common.js
@@ -103,8 +103,6 @@
 
       $("#startYear").html("start year-month: (earliest:" + sTime.slice(0,4) + "-" + sTime.slice(4,6) + ")");
       $("#endYear").html("end year-month: (latest:" + eTime.slice(0,4) + "-" + eTime.slice(4,6) + ")");
-      //$("#startYear").html("start year-month: (earliest:" + sTime + ")");
-      //$("#endYear").html("end year-month: (latest:" + eTime + ")");
     }
 
     function time_range3() {
@@ -135,4 +133,128 @@
       //$("#endYear").html("end year-month: (latest:" + eTime + ")");
     }
 
+var monthList = [
+"Jan",
+"Feb",
+"Mar",
+"Apr",
+"May",
+"Jun",
+"Jul",
+"Aug",
+"Sep",
+"Oct",
+"Nov",
+"Dec",
+];
+
+  function fillMonth() { 
+    var temp1 = 'select months:\
+<select name="months" id="months" onchange="select_months()">\
+<option id="all">select all</option>\
+<option id="none">select none</option>\
+<option id="summer">Summer:Jun-Jul-Aug</option>\
+<option id="autum">Autumn:Sep-Oct-Nov</option>\
+<option id="winter">Winter:Dec-Jan-Feb</option>\
+<option id="spring">Spring:Mar-Apr-May</option> </select>';
+    $("#monthSelect0").html(temp1); 
+
+    temp1 = ""; 
+    for (var i=0; i<monthList.length; i++) {
+      temp1 +=
+         '<input type="checkbox" id="' + monthList[i] + '" value="' + monthList[i] + '"/>' 
+            +  monthList[i] + " ";
+    }
+    $("#monthSelect").html(temp1); 
+  }
+
+    // unselect all months in the checkboxes
+    function reset_months()
+    {
+      for (var i=0; i<monthList.length; i++) {
+        document.getElementById(monthList[i]).checked = false;
+      }
+    }
+
+    // see if no month is selected
+    function no_month_check()
+    {
+      var nonChecked = true;
+      for (var i=0; i<monthList.length; i++) {
+        if (document.getElementById(monthList[i]).checked == true) {
+          nonChecked = false;
+        }
+      }
+      return nonChecked;
+    }
+
+    // select all months in the checkboxes
+    function select_all_months()
+    {
+      for (var i=0; i<monthList.length; i++) {
+        document.getElementById(monthList[i]).checked = true;
+      }
+    }
+
+    // select checkboxes based on "months" dropdown
+    function select_months()
+    {
+      var s1=document.getElementById("months");
+      // alert(s1.selectedIndex);
+      // alert(s1.options[s1.selectedIndex].value);
+
+      // disable the download button because of this change
+      disable_download_button();
+
+      // "select none"
+      if (s1.selectedIndex == 1) {
+        reset_months();
+      }
+      // "select all"
+      if (s1.selectedIndex == 0) {
+        select_all_months();
+      }
+      // "summer"
+      if (s1.selectedIndex == 2) {
+        reset_months();
+        document.getElementById('Jun').checked = true;
+        document.getElementById('Jul').checked = true;
+        document.getElementById('Aug').checked = true;
+      }
+      // "autumn"
+      if (s1.selectedIndex == 3) {
+        reset_months();
+        document.getElementById('Sep').checked = true;
+        document.getElementById('Oct').checked = true;
+        document.getElementById('Nov').checked = true;
+      }
+      // "winter"
+      if (s1.selectedIndex == 4) {
+        reset_months();
+        document.getElementById('Dec').checked = true;
+        document.getElementById('Jan').checked = true;
+        document.getElementById('Feb').checked = true;
+      }
+      // "spring"
+      if (s1.selectedIndex == 5) {
+        reset_months();
+        document.getElementById('Mar').checked = true;
+        document.getElementById('Apr').checked = true;
+        document.getElementById('May').checked = true;
+      }
+
+    }
+
+function getMonthStr() {
+        // get months checked by client
+        var month_str = "";
+        for (var i=0; i<monthList.length; i++) {
+          var mm = document.getElementById(monthList[i]);
+          if (mm.checked == true) {
+            month_str += ","+i+1;
+          }
+        }
+        month_str = month_str.substr(1);
+        return month_str;
+}
 
