ab536972d011fd89dc286bff95040afd4b6ee07d
leipan <lei.pan@jpl.nasa.gov>
2015-08-20 01:31:35 +0000

    added new service Conditional Sampling for Two Variables
    git-svn-id: file:///repo/trunk@946 7b6b0737-f4d6-4764-b6e6-6b822f93333f

--- web_portal/cmac/index.php
+++ web_portal/cmac/index.php
@@ -187,6 +187,10 @@ For a detailed description of the datasets, please click here.
 <img src="images/service.jpeg" alt="service" height="60" width="60">
 <a href="/cmac/web/correlationMap.html">Correlation Map Service</a>.
 </p>
+<p>
+<img src="images/service.jpeg" alt="service" height="60" width="60">
+<a href="/cmac/web/conditionalSampling2Var.html">Conditional Sampling for Two Variables Service</a>.
+</p>
 <ol>
 
 <?php require_once("./footer.php") ?>
