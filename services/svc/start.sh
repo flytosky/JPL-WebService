7a8d4dee13d1332c3472aab194922d4e9f783f3c
leipan <lei.pan@jpl.nasa.gov>
2015-10-15 22:54:32 +0000

    explicitly made timeout to be 300s; used 6 parallel processes
    git-svn-id: file:///repo/trunk@1028 7b6b0737-f4d6-4764-b6e6-6b822f93333f

--- services/svc/start.sh
+++ services/svc/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gunicorn -w2 -b 0.0.0.0:8890 -k tornado --daemon -p svc.pid svc:app
+gunicorn -w6 --timeout 300 --graceful-timeout 300 -b 0.0.0.0:8890 -k tornado --daemon -p svc.pid svc:app
