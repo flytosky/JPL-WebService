6c3f896149d920a18f4112673592890de7e81d74
drseungwonlee <Seungwon.Lee@jpl.nasa.gov>
2015-10-15 20:59:27 +0000

    added image files and topic pdf files
    git-svn-id: file:///repo/trunk@1025 7b6b0737-f4d6-4764-b6e6-6b822f93333f

---
+++ web_portal/cmac/cmda_services.php
@@ -0,0 +1,64 @@
+<?php require_once("./header.php") ?>
+
+<h1>
+Climate Model Diagnostic Analyzer Services
+</h1>
+
+<br />
+
+<ol>
+<p>
+<img src="images/service.jpeg" alt="service" height="60" width="60">
+<a href="/cmac/web/twoDimMap.html">2D Variable Map Service</a>.
+</p>
+<p>
+<img src="images/service.jpeg" alt="service" height="60" width="60">
+<a href="/cmac/web/twoDimZonalMean.html">2D Variable Zonal Mean Service</a>.
+</p>
+<p>
+<img src="images/service.jpeg" alt="service" height="60" width="60">
+<a href="/cmac/web/twoDimTimeSeries.html">2D Variable Time Series Service</a>.
+</p>
+<p>
+<img src="images/service.jpeg" alt="service" height="60" width="60">
+<a href="/cmac/web/twoDimSlice3D.html">3D Variable 2D Slice Service</a>.
+</p>
+<p>
+<img src="images/service.jpeg" alt="service" height="60" width="60">
+<a href="/cmac/web/threeDimZonalMean.html">3D Variable Zonal Mean Service</a>.
+</p>
+<p>
+<img src="images/service.jpeg" alt="service" height="60" width="60">
+<a href="/cmac/web/threeDimVarVertical.html">3D Variable Average Vertical Profile Service</a>.
+</p>
+<p>
+<img src="images/service.jpeg" alt="service" height="60" width="60">
+<a href="/cmac/web/scatterPlot2Vars.html">Scatter and Histogram Plots of Two Variables Service</a>.
+</p>
+<p>
+<img src="images/service.jpeg" alt="service" height="60" width="60">
+<a href="/cmac/web/diffPlot2Vars.html">Difference Plot of Two Variables Service</a>.
+</p>
+<p>
+<img src="images/service.jpeg" alt="service" height="60" width="60">
+<a href="/cmac/web/correlationMap.html">Time-lagged Correlation Map Service</a>.
+</p>
+<p>
+<img src="images/service.jpeg" alt="service" height="60" width="60">
+<a href="/cmac/web/conditionalSampling.html">Conditional Sampling with One Variable Service</a>.
+</p>
+<p>
+<img src="images/service.jpeg" alt="service" height="60" width="60">
+<a href="/cmac/web/conditionalSampling2Var.html">Conditional Sampling with Two Variables Service</a>.
+</p>
+<p>
+<img src="images/service.jpeg" alt="service" height="60" width="60">
+<a href="/cmac/web/regridAndDownload.html">Regrid and Download Service</a>.
+</p>
+<p>
+<img src="images/service.jpeg" alt="service" height="60" width="60">
+<a href="/cmac/web/datasetTable.html">Dataset Search Service</a>.
+</p>
+<ol>
+
+<?php require_once("./footer.php") ?>
